8d04eaa21c418488da8d79b84cf3ebf12385bdf2
toxidity-18 <desavigatunganga@gmail.com>
2025-08-16 00:00:57 +0300

    printing the final_price

--- Result.py
+++ Result.py
@@ -10,6 +10,11 @@ original_price = float(input("Enter the original price of the item: "))
 discount_percent = float(input("Enter the discount percentage: "))
 
 # Calculate final price using the function
+# Calling the function and it parameters , storing the returned value in variable final_price 
 final_price = calculate_discount(original_price, discount_percent)
 
-
+# Print the result based on the final_price value .
+if discount_percent >= 20:
+    print(f"The final price after applying the discount is: {final_price}")
+else:
+    print(f"No discount applied. The price remains: {final_price}")
